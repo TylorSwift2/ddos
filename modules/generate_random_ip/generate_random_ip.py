c74d8479c732f3fec9c5af9d8e73990448374a6c
Tylor swift <roberdoogarcia@gmail.com>
2025-04-26 14:17:02 -0300

    Delete modules directory

--- modules/generate_random_ip/generate_random_ip.py
+++
@@ -1,7 +0,0 @@
-import random
-def generate_random_ip():
-    """
-   Generates a valid random IP address.
-    """
-    return f"{random.randint(1, 255)}.{random.randint(0, 255)}.{random.randint(0, 255)}.{random.randint(0, 255)}"
-# Definition of the test_ddos_with_ips function
